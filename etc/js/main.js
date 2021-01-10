5fdabfdb8183d78d161233dd2844a6e40d097381
aakendev <aakendev@gmail.com>
2021-01-10 23:08:59 +0700

    Adding Second Page

--- etc/js/main.js
+++ etc/js/main.js
@@ -3,11 +3,11 @@ const menuDD = document.querySelector("#menu-dd");
 
 /* DROPDOWN FUNCTIONS */
 btnDD.onclick = () => {
-    if(menuDD.className !== "bg-green"){
-        btnDD.classList.replace("fa-folder-open", "fa-folder");
-        menuDD.classList.remove("mh-dd");
-    }else{
-        btnDD.classList.replace("fa-folder","fa-folder-open");
-        menuDD.classList.add("mh-dd");
-    }
+  if (menuDD.className !== "bg-green") {
+    btnDD.classList.replace("fa-folder-open", "fa-folder");
+    menuDD.classList.remove("mh-dd");
+  } else {
+    btnDD.classList.replace("fa-folder", "fa-folder-open");
+    menuDD.classList.add("mh-dd");
+  }
 }
